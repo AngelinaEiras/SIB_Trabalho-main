66a9b10be548d5da1f785304ea1f6ed1cdde9551
AngelinaEiras <angie.eiras@gmail.com>
2023-01-10 16:36:13 +0000

    Initial commit

---
+++ kag/exp.py
@@ -0,0 +1,39 @@
+#
+#pip install git+https://github.com/jcapels/propythia.git@fix_dependencies
+#
+#pip install bio-embeddings[all]
+
+
+'''
+Deverá ser elaborado um Jupyter Notebook
+- organizado em secções, que
+inclua os passos da análise realizada, e que explique muito sucintamente os
+procedimentos realizados e decisões tomadas ao longo da análise.
+
+B2. Kaggle - Novozymes enzyme stability prediction -
+https://www.kaggle.com/competitions/novozymes-enzyme-stability-prediction
+
+
+Fase 1:
+Exploração inicial e pré-processamento
+• Rever toda a documentação disponível sobre o conjunto de dados.
+• Carregar o conjunto de dados e realizar uma análise exploratória do mesmo.
+• Realizar os passos necessários de preparação dos dados e pré-processamento,
+incluindo possivelmente a geração de atributos, a sua seleção, o tratamento de
+possíveis valores em falta, etc.
+Esta etapa deve corresponder à secção 1 do Notebook onde deverá:
+• descrever e caracterizar os dados atribuídos de acordo com a documentação/
+literatura existente;
+• descrever sucintamente as características dos dados disponíveis a partir da
+análise exploratória inicial;
+• descrever os passos de preparação dos dados e pré-processamento que efetuou,
+justificando as suas escolhas;
+• incluir os gráficos exploratórios iniciais que ilustrem as principais características
+dos dados.
+
+
+'''
+
+
+
+
